9be81b554fc3411556ad0651d364a1a7dee05503
wpb <wpb.git@xyee.pl>
2019-12-19 22:46:10 +0100

    Module6_end

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,10 +2,21 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { incorrectAnswer: false };
+  }
+
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
+      this.setState(state => {
+        return (this.state.incorrectAnswer = false);
+      });
     }
+    this.setState(state => {
+      return (this.state.incorrectAnswer = true);
+    });
   }
 
   render() {
@@ -29,6 +40,9 @@ class QuizQuestion extends Component {
             )}
           </ul>
         </section>
+        {this.state.incorrectAnswer == true ? (
+          <p className="error">Sorry, that's not right</p>
+        ) : null}
       </main>
     );
   }
