cc051e920e630ff67727f70e229ee2e0cfed774b
wpb <wpb.git@xyee.pl>
2019-12-20 09:08:08 +0100

    Revert "Module6_end"
    This reverts commit 9be81b554fc3411556ad0651d364a1a7dee05503.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,21 +2,10 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { incorrectAnswer: false };
-  }
-
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
-      this.setState(state => {
-        return (this.state.incorrectAnswer = false);
-      });
     }
-    this.setState(state => {
-      return (this.state.incorrectAnswer = true);
-    });
   }
 
   render() {
@@ -40,9 +29,6 @@ class QuizQuestion extends Component {
             )}
           </ul>
         </section>
-        {this.state.incorrectAnswer == true ? (
-          <p className="error">Sorry, that's not right</p>
-        ) : null}
       </main>
     );
   }
